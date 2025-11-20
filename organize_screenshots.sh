8c977bf74f13a85cf2524f1d4c2ec0340b35bee0
ija-ope <ijaolaopemipo@gmail.com>
2025-11-20 21:27:26 +0100

    feat: Search for screenshots in Desktop, Downloads, and Documents

--- organize_screenshots.sh
+++ organize_screenshots.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find /Users/mac/Desktop -name "Screenshot*.png" -mtime +1 -exec mv {} /Users/mac/Documents/old\ files/ \;
+find /Users/mac/Desktop /Users/mac/Downloads /Users/mac/Documents -name "Screenshot*.png" -mtime +1 -exec mv {} /Users/mac/Documents/old\ files/ \;
